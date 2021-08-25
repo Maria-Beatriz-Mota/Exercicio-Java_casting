7691269adb07367c99c7cd6db847e55ae04268bb
Maria Beatriz Mota <mbeatriz.mbia@gmail.com>
2021-08-25 18:00:27 -0300

    Aprendendo-Casting.java

---
+++ casting.java
@@ -0,0 +1,11 @@
+public class casting {
+	
+	public static void main(String[] args) {
+
+		double Combo = 25.01;
+		
+		int Valor = (int) Combo; //casting//
+		
+		System.out.println("o valor do eu combo é: " + Valor + "R$");
+	}
+}
